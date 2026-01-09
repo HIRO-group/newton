fca61ec321e94b4b5f81751df3a266d5fba77992
ava <ava.abderezaei@gmail.com>
2026-01-09 08:12:34 -0700

    Finalize PR

--- newton/_src/solvers/srxpbd/solver_srxpbd.py
+++ newton/_src/solvers/srxpbd/solver_srxpbd.py
@@ -1,18 +1,3 @@
-# SPDX-FileCopyrightText: Copyright (c) 2025 The Newton Developers
-# SPDX-License-Identifier: Apache-2.0
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import warp as wp
 
 from ...core.types import override
@@ -20,38 +5,16 @@ from ...sim import Contacts, Control, Model, State
 from ..solver import SolverBase
 from .kernels import (
     apply_particle_deltas,
-    apply_particle_shape_restitution,
-    bending_constraint,
     solve_particle_shape_contacts,
-    solve_particle_particle_contacts,
-    solve_springs,
-    solve_tetrahedra,
     solve_shape_matching,
 )
 
 
 class SolverSRXPBD(SolverBase):
-    """An implicit integrator using eXtended Position-Based Dynamics (XPBD) for rigid and soft body simulation.
-
-    References:
-        - Miles Macklin, Matthias Müller, and Nuttapong Chentanez. 2016. XPBD: position-based simulation of compliant constrained dynamics. In Proceedings of the 9th International Conference on Motion in Games (MIG '16). Association for Computing Machinery, New York, NY, USA, 49-54. https://doi.org/10.1145/2994258.2994272
-        - Matthias Müller, Miles Macklin, Nuttapong Chentanez, Stefan Jeschke, and Tae-Yong Kim. 2020. Detailed rigid body simulation with extended position based dynamics. In Proceedings of the ACM SIGGRAPH/Eurographics Symposium on Computer Animation (SCA '20). Eurographics Association, Goslar, DEU, Article 10, 1-12. https://doi.org/10.1111/cgf.14105
-
-    After constructing :class:`Model`, :class:`State`, and :class:`Control` (optional) objects, this time-integrator
-    may be used to advance the simulation state forward in time.
-
-    Example
-    -------
-
-    .. code-block:: python
-
-        solver = newton.solvers.SolverXPBD(model)
-
-        # simulation loop
-        for i in range(100):
-            solver.step(state_in, state_out, control, contacts, dt)
-            state_in, state_out = state_out, state_in
-
+    """
+    Similar to SolverXPBD. Only includes contact handling + shape matching constraints.
+    This solver assumes complete rigid bodies. No soft bodies.
+    Rigid bodies are modeled as a collection of particles.
     """
 
     def __init__(
